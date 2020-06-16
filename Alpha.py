1adc3cbe95c05142e107f7875d4767c9fb53628f
Dylanyessid <dylanyessid1904@gmail.com>
2020-06-16 11:40:04 -0500

    Ventana

--- Alpha.py
+++ Alpha.py
@@ -1,1 +1,12 @@
 import pygame
+pygame.init()
+
+size=(800,600)
+screen=pygame.display.set_mode(size)
+
+
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
